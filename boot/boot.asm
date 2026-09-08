b3f66197c30d65e83d2e6ed1b7ca899d22f0e88f
akshatidk29 <akshatmittal2024@gmail.com>
2026-09-08 17:09:44 +0530

    Fix some segment intialization issues and add comments

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,32 +1,25 @@
 BITS 16
 ORG 0x7C00
 
-start:
+    ; Initialize Segments
+    xor ax, ax              ; because we cant load an immediate value into a segment register directly
+    mov ds, ax              ; data segment
+    mov es, ax              ; extra segment
     
-    mov [boot_drive], dl  ; Save the boot drive number
+    mov [boot_drive], dl    ; Save boot drive (BIOS puts it in dl)
 
-    ; Load kernel from disk
+    ; Read Sector 2 into 0x0000:0x500   [Because the 0x000:0x500 is used/reserved by IVT + BDA]
 
-    mov ah, 0x02         ; BIOS: read sectors
-    mov al, 1            ; Read 1 sector
+    mov ah, 0x02            ; Read Sectors
+    mov al, 1               ; Number of Sectors
+    mov ch, 0               ; Cylinder
+    mov cl, 2               ; Sector (sector 2 is kernel)
+    mov dh, 0               ; Head
+    mov dl, [boot_drive]    ; Save Boot Drive
+    mov bx, 0x0500          ; ES:BX = 0x0000:0x500
+    int 0x13                ; ES:BX = 0x0000:0x500
 
-    mov ch, 0            ; Cylinder 0
-    mov cl, 2            ; Sector 2
-    mov dh, 0            ; Head 0
-    mov dl, [boot_drive]
-
-    ; Destination = 0x10000
-    mov ax, 0x1000
-    mov es, ax
-    mov bx, 0x0000
-
-    int 0x13
-
-    ; If BIOS returns here, reading succeeded
-
-    ; Jump to loaded kernel
-    jmp 0x1000:0x0000
-
+    jmp 0x0000:0x0500       ; Jump to Kernel
 
 boot_drive:
     db 0
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,23 +1,24 @@
 BITS 16
+ORG 0x0500          ; Loaded at 0x0000:0x0500
 
-start:
+    xor ax, ax
+    mov ds, ax      ; DS = 0 
     mov si, message
 
-print:
-    lodsb
+.print:
+    lodsb           ; Load byte from [DS:SI] into AL, Also increments the SI
+    cmp al, 0       ; AL = Character to Display
+    je .hang        ; if AL is 0, jump to .hang
+    mov ah, 0x0E    ; AH = Video Mode, 0x0E means Display a Character
+    mov bh, 0       ; BH = Page Number
+    int 0x10        ; Interrupt for Teleoperation
+    jmp .print
 
-    cmp al, 0
-    je hang
-
-    mov ah, 0x0E
-    int 0x10
-
-    jmp print
-
-hang:
-    cli
+.hang:
     hlt
-    jmp hang
+    jmp .hang
 
 message:
     db "Hello from kernel!", 0
+
+times 512 - ($ - $$) db 0
