b0309664205d541ffb0dbd145d604c6ae5e144e4
akshatidk29 <akshatmittal2024@gmail.com>
2026-09-12 01:42:41 +0530

    Add code to change from real to protected mode

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,28 +1,97 @@
 BITS 16
 ORG 0x7C00
 
-    ; Initialize Segments
-    xor ax, ax              ; because we cant load an immediate value into a segment register directly
-    mov ds, ax              ; data segment
-    mov es, ax              ; extra segment
+; Initialize Segments
+    xor ax, ax                              ; because we cant load an immediate value into a segment register directly
+    mov ds, ax                              ; data segment
+    mov es, ax                              ; extra segment
     
-    mov [boot_drive], dl    ; Save boot drive (BIOS puts it in dl)
+    mov [bootDriveVariable], dl             ; Save boot drive (BIOS puts it in dl)
 
-    ; Read Sector 2 into 0x0000:0x500   [Because the 0x000:0x500 is used/reserved by IVT + BDA]
+; Read Sector 2 into 0x0000:0x0500   [Because the 0x000:0x0500 is used/reserved by IVT + BDA]
 
-    mov ah, 0x02            ; Read Sectors
-    mov al, 1               ; Number of Sectors
-    mov ch, 0               ; Cylinder
-    mov cl, 2               ; Sector (sector 2 is kernel)
-    mov dh, 0               ; Head
-    mov dl, [boot_drive]    ; Save Boot Drive
-    mov bx, 0x0500          ; ES:BX = 0x0000:0x500
-    int 0x13                ; ES:BX = 0x0000:0x500
+    mov ah, 0x02                            ; Read Sectors
+    mov al, 1                               ; Number of Sectors
+    mov ch, 0                               ; Cylinder
+    mov cl, 2                               ; Sector (sector 2 is kernel)
+    mov dh, 0                               ; Head
+    mov dl, [bootDriveVariable]             ; Save Boot Drive
+    mov bx, 0x0500                          ; ES:BX = 0x0000:0x0500
+    int 0x13                                ; ES:BX = 0x0000:0x0500
 
-    jmp 0x0000:0x0500       ; Jump to Kernel
 
-boot_drive:
+; Changing System from Real Mode to Protected Mode
+
+    cli                                     ; Disable Interrupts
+
+; Load GDT
+    lgdt [gdtDescriptor]
+
+; Set PE bit in CR0
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax                            ; PE bit enables Protected Mode
+
+; Far Jump into Protected Mode
+    jmp 0x08:protectedMode                  ; CS:IP = 0x08:0x0500
+
+
+
+
+
+
+
+; 32-bit Code
+
+BITS 32
+
+protectedMode:
+    jmp 0x08:0x0500                         ; Jump to Kernel
+
+
+
+
+
+
+
+; Data Variables
+
+BITS 16
+
+bootDriveVariable:
     db 0
 
+
+; GDT
+
+gdtStart:
+
+gdtNull:
+    dq 0x0000000000000000                   ; 1st Entry is Null
+
+gdtCode:                                    ; Code Segment
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xCF
+    db 0x00
+
+gdtData:                                    ; Data Segment
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xCF
+    db 0x00
+
+gdtEnd:
+
+gdtDescriptor:
+    dw gdtEnd - gdtStart - 1
+    dd gdtStart
+
+
 times 510 - ($ - $$) db 0
+
 dw 0xAA55
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,17 +1,23 @@
-BITS 16
-ORG 0x0500          ; Loaded at 0x0000:0x0500
+BITS 32
+ORG 0x0500                      ; Loaded at 0x0000:0x0500
+    
+    mov ax, 0x10                ; Data Segment Selector
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov fs, ax
+    mov gs, ax
 
-    xor ax, ax
-    mov ds, ax      ; DS = 0 
-    mov si, message
+    mov esi, message
+    mov edi, 0xB8000            ; VGA text buffer (physical address)
+    mov ah, 0x0F                ; Attribute: white text on black background
 
 .print:
-    lodsb           ; Load byte from [DS:SI] into AL, Also increments the SI
-    cmp al, 0       ; AL = Character to Display
-    je .hang        ; if AL is 0, jump to .hang
-    mov ah, 0x0E    ; AH = Video Mode, 0x0E means Display a Character
-    mov bh, 0       ; BH = Page Number
-    int 0x10        ; Interrupt for Teleoperation
+    lodsb                       ; Load byte from [DS:ESI] into AL, increment ESI
+    cmp al, 0                   ; Check for null terminator
+    je .hang
+    mov [edi], ax               ; Write char (AL) + attribute (AH) to VGA buffer
+    add edi, 2                  ; Each VGA cell = 2 bytes (char + color)
     jmp .print
 
 .hang:
