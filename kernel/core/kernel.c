cabfbbf4baa8081a87aa651a5da302c2eb9ddd4e
akshatidk29 <akshatmittal2024@gmail.com>
2026-09-14 18:18:33 +0530

    Hardcode KANHA to screen

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -1,7 +1,23 @@
-char data[1024] = {1, 2, 3, 4};
-
 void kernel_main(void)
 {
+    volatile unsigned char *video =
+        (volatile unsigned char *)0xB8000;
+
+    video[0] = 'K';
+    video[1] = 0x07;
+
+    video[2] = 'A';
+    video[3] = 0x07;
+
+    video[4] = 'N';
+    video[5] = 0x07;
+
+    video[6] = 'H';
+    video[7] = 0x07;
+
+    video[8] = 'A';
+    video[9] = 0x07;
+
     while (1) {
     }
 }
