9c0a7f744b7a16218520abf05832fd2ee046fe7e
akshatidk29 <akshatmittal2024@gmail.com>
2026-09-20 13:02:31 +0530

    Add vga cursor which is controlled by CRT controller

--- kernel/drivers/vga/vga.c
+++ kernel/drivers/vga/vga.c
@@ -1,35 +1,50 @@
+#include "io.h"
 #include "vga.h"
 
 #define VGA_WIDTH  80
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((volatile unsigned char *)0xB8000)
 
-static unsigned int row;
-static unsigned int column;
+
+#define VGA_CRTC_DATA 0x3D5             // VGA Cathode Ray Tube Controller for Blinking Cursor - Data Register
+#define VGA_CRTC_INDEX 0x3D4            // VGA Cathode Ray Tube Controller for Blinking Cursor - Index Register
+
+#define VGA_CRTC_CURSOR_LOW 0x0F        // Blinking Cursor Position - Low Byte
+#define VGA_CRTC_CURSOR_HIGH 0x0E       // Blinking Cursor Position - High Byte
+
+static uint32_t row;
+static uint32_t column;
 
 void vga_initialize(void){
     row = 0;
     column = 0;
 
-    for (unsigned int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+    for (uint32_t i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
         VGA_MEMORY[i * 2] = ' ';
         VGA_MEMORY[i * 2 + 1] = 0x07;
     }
 }
 
 void vga_put_char(char c){   
+
+    uint32_t pos = (row * VGA_WIDTH + column) * 2;
+    
     if(c == '\n'){
+
+        vga_clear_char();
+        
         column = 0;
         row++;
 
         if(row >= VGA_HEIGHT){
             vga_scroll();
         }
+        vga_cursor();
         return;
     }
-
-    VGA_MEMORY[(row * VGA_WIDTH + column) * 2] = c;
-    VGA_MEMORY[(row * VGA_WIDTH + column) * 2 + 1] = 0x07;
+    
+    VGA_MEMORY[pos] = c;
+    VGA_MEMORY[pos + 1] = 0x07;
 
     column++;
 
@@ -41,7 +56,33 @@ void vga_put_char(char c){
             vga_scroll();
         }
     }
+
+    vga_cursor();
 }
+
+void vga_clear_char(void){
+    uint32_t pos = (row * VGA_WIDTH + column) * 2;
+    VGA_MEMORY[pos] = ' ';
+    VGA_MEMORY[pos + 1] = 0x07;
+}
+
+
+void vga_set_cursor(uint8_t row, uint8_t column) {
+    uint16_t pos = row * VGA_WIDTH + column;
+
+    // Low byte
+    outb(VGA_CRTC_INDEX, VGA_CRTC_CURSOR_LOW);
+    outb(VGA_CRTC_DATA, (uint8_t)(pos & 0xFF));
+
+    // High byte
+    outb(VGA_CRTC_INDEX, VGA_CRTC_CURSOR_HIGH);
+    outb(VGA_CRTC_DATA, (uint8_t)((pos >> 8) & 0xFF));
+}
+
+void vga_cursor(void){
+    vga_set_cursor(row, column);
+}
+    
 
 void vga_put_int(uint32_t n){
 
@@ -73,21 +114,20 @@ void vga_write(const char* c){
     }
 }
 
-
 void vga_scroll(void){
     
-    for(int i = 1; i < VGA_HEIGHT; i++){
-        for(int j = 0; j < VGA_WIDTH; j++){
+    for(uint32_t i = 1; i < VGA_HEIGHT; i++){
+        for(uint32_t j = 0; j < VGA_WIDTH; j++){
 
-            unsigned int src = (i * VGA_WIDTH + j) * 2;
-            unsigned int dst = ((i - 1) * VGA_WIDTH + j) * 2;
+            uint32_t src = (i * VGA_WIDTH + j) * 2;
+            uint32_t dst = ((i - 1) * VGA_WIDTH + j) * 2;
 
             VGA_MEMORY[dst] = VGA_MEMORY[src];
             VGA_MEMORY[dst + 1] = VGA_MEMORY[src + 1];
         }
     }
 
-    for(int j = 0; j < VGA_WIDTH; j++){
+    for(uint32_t j = 0; j < VGA_WIDTH; j++){
         VGA_MEMORY[(VGA_HEIGHT - 1) * VGA_WIDTH * 2 + j * 2] = ' ';
         VGA_MEMORY[(VGA_HEIGHT - 1) * VGA_WIDTH * 2 + j * 2 + 1] = 0x07;
     }
--- kernel/drivers/vga/vga.h
+++ kernel/drivers/vga/vga.h
@@ -5,7 +5,12 @@
 
 void vga_initialize();
 
+void vga_set_cursor(uint8_t row, uint8_t column);
+void vga_cursor(void);
+
 void vga_put_char(char c);
+void vga_clear_char(void);
+
 void vga_write(const char* c);
 
 void vga_put_int(uint32_t n);
